88e5fdbaca9dd52c592d181c8c4b2981b3c8f269
Lovanto R <rifkylovanto@gmail.com>
2022-03-07 18:53:31 +0700

    Revert "Create constant.dart"
    This reverts commit 2b276c05bca7c99a7608862b530ff6961fa7bcc0.

--- lib/constant/constant.dart
+++
@@ -1,5 +0,0 @@
-String ANIMATED_SPLASH = '/SplashScreen',
-    HOME_SCREEN = '/HomeScreen',
-    PHOTO_CONTAINER_SCREEN = '/PhotoContainerScreen',
-    VIDEO_CONTAINER_SCREEN = '/VideoContainerScreen',
-    ALBUM_CONTAINER_SCREEN = '/AlbumContainerScreen';
