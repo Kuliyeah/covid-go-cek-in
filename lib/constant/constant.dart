591d5916c7562ccd5117c7701da893f844ead48b
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-03-08 20:48:24 +0700

    color, main dart

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -1,5 +1,16 @@
+import 'package:flutter/material.dart';
+
 String ANIMATED_SPLASH = '/SplashScreen',
     HOME_SCREEN = '/HomeScreen',
     PHOTO_CONTAINER_SCREEN = '/PhotoContainerScreen',
     VIDEO_CONTAINER_SCREEN = '/VideoContainerScreen',
     ALBUM_CONTAINER_SCREEN = '/AlbumContainerScreen';
+//
+Color mainColor = Color(0xFF5DD482);
+Color splashBgColor = Color(0xFFCAFFDA);
+Color grTopLeftColor = Color(0xFFF3F3F3);
+Color grBottomRightColor = Color(0xFFE3FFF6);
+Color textColor = Color(0xFF29343D);
+Color bodyColor = Color(0xFF888888);
+Color warningColor = Color(0xFFF6C000);
+Color dangerColor = Color(0xFFFF6969);
--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "GoCheck-in",
       theme: ThemeData(primarySwatch: Colors.green, fontFamily: 'Lato'),
       home: SplashScreenPage(),
