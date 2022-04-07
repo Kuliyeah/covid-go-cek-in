9638fead1ba8890e4cf349f46b9fd8d677e43f5d
Dzakwan Daffa Hidayatullah <dzakwandh9@gmail.com>
2022-04-07 09:43:20 +0700

    Update term_of_service_page.dart

--- lib/view/screen/account_screen/term_of_service_screen/term_of_service_page.dart
+++ lib/view/screen/account_screen/term_of_service_screen/term_of_service_page.dart
@@ -27,6 +27,43 @@ class _Term_Of_Service_pageState extends State<Term_Of_Service_Page> {
                 dividerColor: Colors.red,
                 expandedHeaderPadding: EdgeInsets.only(bottom: 0.0),
                 elevation: 1,
+                children: [
+                  ExpansionPanel(
+                    headerBuilder: (BuildContext context, bool isExpanded) {
+                      return Container(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          itemData[index].headerItem,
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      );
+                    },
+                    body: Container(
+                      padding: EdgeInsets.only(left: 10, right: 10, bottom: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            itemData[index].discription,
+                            style: TextStyle(
+                              color: Colors.grey[700],
+                              fontSize: 15,
+                              height: 1.3,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    isExpanded: itemData[index].expanded,
+                  )
+                ],
+                expansionCallback: (int item, bool status) {
+                  setState(() {
+                    itemData[index].expanded = !itemData[index].expanded;
+                  });
+                },
               ),
             );
           },
