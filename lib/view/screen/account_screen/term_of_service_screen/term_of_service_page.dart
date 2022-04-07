0d01e603f206dd1c5d5d7826db5ba31975cc7342
Dzakwan Daffa Hidayatullah <dzakwandh9@gmail.com>
2022-04-07 09:42:00 +0700

    Update term_of_service_page.dart

--- lib/view/screen/account_screen/term_of_service_screen/term_of_service_page.dart
+++ lib/view/screen/account_screen/term_of_service_screen/term_of_service_page.dart
@@ -10,7 +10,30 @@ class Term_Of_Service_Page extends StatefulWidget {
 class _Term_Of_Service_pageState extends State<Term_Of_Service_Page> {
   List<ItemModel> itemData = <ItemModel>[];
   @override
-  
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Term Of Service"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: ListView.builder(
+          itemCount: itemData.length,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: EdgeInsets.only(bottom: 10.0),
+              child: ExpansionPanelList(
+                animationDuration: Duration(milliseconds: 500),
+                dividerColor: Colors.red,
+                expandedHeaderPadding: EdgeInsets.only(bottom: 0.0),
+                elevation: 1,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
 }
 
 class ItemModel {
