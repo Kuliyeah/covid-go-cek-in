58040e7f4fa6fa95e3aabfced4a3ce253c9c501b
Lovanto R <rifkylovanto@gmail.com>
2022-04-10 16:00:27 +0700

    Create Kunjungan.dart

---
+++ lib/models/Kunjungan.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: file_names
+
+class Mitra {
+  Mitra({
+    required this.idMitra,
+    required this.namaMitra,
+    required this.alamatMitra,
+    required this.noHpMitra,
+    required this.jenisUsaha,
+    required this.deskripsi,
+    required this.fotoUsaha,
+    required this.status,
+  });
+
+  final int? idMitra;
+  final String? namaMitra;
+  final String? alamatMitra;
+  final String? noHpMitra;
+  final String? jenisUsaha;
+  final String? deskripsi;
+  final String? fotoUsaha;
+  final String? status;
+
+  factory Mitra.fromJson(Map<String, dynamic> json) {
+    return Mitra(
+      idMitra: json['data']['idMitra'],
+      namaMitra: json['data']['namaMitra'],
+      alamatMitra: json['data']['alamatMitra'],
+      noHpMitra: json['data']['noHpMitra'],
+      jenisUsaha: json['data']['jenisUsaha'],
+      deskripsi: json['data']['deskripsi'],
+      fotoUsaha: json['data']['fotoUsaha'],
+      status: json['data']['fotoUsaha'],
+    );
+  }
+}
