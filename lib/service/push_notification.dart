fd1b183f086a4b422d412199e1010251f2c63cb9
Lovanto R <rifkylovanto@gmail.com>
2022-06-05 22:01:18 +0700

    Update push_notification.dart

--- lib/service/push_notification.dart
+++ lib/service/push_notification.dart
@@ -5,57 +5,59 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class PushNotification {
+  void tes(AndroidNotificationChannel? channel,
+      FlutterLocalNotificationsPlugin? flutterLocalNotificationsPlugin) async {
+    if (!kIsWeb) {
+      channel = const AndroidNotificationChannel(
+        'high_importance_channel', // id
+        'High Importance Notifications', // title
+        importance: Importance.max,
+      );
 
-void tes(AndroidNotificationChannel? channel,
-    FlutterLocalNotificationsPlugin? flutterLocalNotificationsPlugin) async {
-  if (!kIsWeb) {
-    channel = const AndroidNotificationChannel(
-      'high_importance_channel', // id
-      'High Importance Notifications', // title
+      flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
-      importance: Importance.max,
-    );
-
-    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-
-    await flutterLocalNotificationsPlugin
-        .resolvePlatformSpecificImplementation<
-            AndroidFlutterLocalNotificationsPlugin>()
-        ?.createNotificationChannel(channel);
-    await FirebaseMessaging.instance
-        .setForegroundNotificationPresentationOptions(
-      alert: true,
-      badge: true,
-      sound: true,
-    );
-  }
-}
-
-void listenMessage(AndroidNotificationChannel? channel,
-    FlutterLocalNotificationsPlugin? flutterLocalNotificationsPlugin) {
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    RemoteNotification? notification = message.notification;
-    AndroidNotification? android = message.notification?.android;
-    if (notification != null && android != null && !kIsWeb) {
-      flutterLocalNotificationsPlugin?.show(
-        notification.hashCode,
-        notification.title,
-        notification.body,
-        NotificationDetails(
-          android: AndroidNotificationDetails(
-            channel!.id,
-            channel.name,
-            icon: 'launch_background',
-          ),
-        ),
+      await flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<
+              AndroidFlutterLocalNotificationsPlugin>()
+          ?.createNotificationChannel(channel);
+      await FirebaseMessaging.instance
+          .setForegroundNotificationPresentationOptions(
+        alert: true,
+        badge: true,
+        sound: true,
       );
     }
-  });
-}
+  }
 
-void msgOpenedApp() {
-  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-    print('A new onMessageOpenedApp event was published!');
-  });
-}
+  void listenMessage(AndroidNotificationChannel? channel,
+      FlutterLocalNotificationsPlugin? flutterLocalNotificationsPlugin) {
+    FirebaseMessaging.onMessage.listen(
+      (RemoteMessage message) {
+        RemoteNotification? notification = message.notification;
+        AndroidNotification? android = message.notification?.android;
+        if (notification != null && android != null && !kIsWeb) {
+          flutterLocalNotificationsPlugin?.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel!.id,
+                channel.name,
+                icon: 'launch_background',
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  void msgOpenedApp() {
+    FirebaseMessaging.onMessageOpenedApp.listen(
+      (RemoteMessage message) {
+        print('A new onMessageOpenedApp event was published!');
+      },
+    );
+  }
 }
