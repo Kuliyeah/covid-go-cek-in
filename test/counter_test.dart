64d1cff29aa824a43c3ce081f2c8172992198bca
Dzakwan Daffa Hidayatullah <dzakwandh9@gmail.com>
2022-06-08 21:55:10 +0700

    create and update test

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -1,14 +1,17 @@
+import 'package:covid_go_cek_in/helperurl.dart';
 import 'package:covid_go_cek_in/view/screen/scan_screen/scan_page.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('Scanner QR Code', () {
-    test('QR code must contain "gocekin"', () {
-      expect(validationData("gocekin-20"), true);
-    });
-    test('Get id mitra from scanned QR Code', () {
-      expect(getIDMitraFromScanner("gocekin-20"), "20");
-    });
-    test('Kode QR ', () {});
+  test('QR code must contain "gocekin"', () {
+    expect(validationData("gocekin-20."), true);
+  });
+
+  test('Get id mitra from scanned QR Code', () {
+    expect(getIDMitraFromScanner("gocekin-2."), "2");
+  });
+
+  test('Get url and port', () {
+    expect(MyUrl().getUrl(), "http://192.168.1.102:8787");
   });
 }
